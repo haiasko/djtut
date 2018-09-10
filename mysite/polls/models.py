6f0c797ab1e30ad1bd44c7564462137fc682007c
Plamen Nikolaev <plamencurso@gmail.com>
2018-09-10 20:04:33 +0000

    fixed misplaced method in Question

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -11,7 +11,8 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
-
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
@@ -20,5 +21,3 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
