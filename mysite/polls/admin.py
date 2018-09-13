3def78e38dbfbaa47a89b960719b4f18c05105c8
Plamen Nikolaev <plamencurso@gmail.com>
2018-09-13 12:52:22 +0000

    Added admin class to reorder fields of Question

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Choice, Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
